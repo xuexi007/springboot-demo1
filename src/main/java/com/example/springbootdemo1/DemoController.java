accbd00adb876f5a3da1216796b0e4ace24f9333
java1105 <hk109@126.com>
2019-04-11 16:33:17 +0800

    这是一个java项目，广州团队研发

---
+++ src/main/java/com/example/springbootdemo1/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.springbootdemo1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+
+        return "Hello JAVA1105!";
+    }
+}
